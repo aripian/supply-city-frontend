d19634aef57828eaba5c026778f5648f946383cc
Nik Faris <nikfaris@stackideas.com>
2018-08-13 22:43:10 +0800

    Added views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,31 +10,56 @@
  */
 angular
   .module('supplyFrontApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
+	'ngAnimate',
+	'ngCookies',
+	'ngResource',
+	'ngRoute',
+	'ngSanitize',
+	'ngTouch'
   ])
   .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/tables', {
-        templateUrl: 'views/tables.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+	$routeProvider
+	  .when('/', {
+		templateUrl: 'views/main.html',
+		controller: 'MainCtrl',
+		controllerAs: 'main'
+	  })
+	  .when('/tables', {
+		templateUrl: 'views/tables.html',
+		controller: 'AboutCtrl',
+		controllerAs: 'about'
+	  })
+	  .when('/users', {
+		templateUrl: 'views/users.html',
+		controller: 'usersCtrl',
+		controllerAs: 'users'
+	  })
+	  .when('/roles', {
+		templateUrl: 'views/roles.html',
+		controller: 'rolesCtrl',
+		controllerAs: 'roles'
+	  })
+	  .when('/industry', {
+		templateUrl: 'views/industry.html',
+		controller: 'industrytCtrl',
+		controllerAs: 'industry'
+	  })
+	  .when('/news', {
+		templateUrl: 'views/news.html',
+		controller: 'newstCtrl',
+		controllerAs: 'news'
+	  })
+	  .when('/forms', {
+		templateUrl: 'views/forms.html',
+		controller: 'AboutCtrl',
+		controllerAs: 'forms'
+	  })
+	  .otherwise({
+		redirectTo: '/'
+	  });
   })
   .config(['$locationProvider', function($locationProvider) {
 
-    $locationProvider.hashPrefix('');
+	$locationProvider.hashPrefix('');
 
   }]);
