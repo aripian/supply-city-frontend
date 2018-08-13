bbeabca2ee128fa7c67c6c71d004023f9415f2df
Muhammad Nor Arif Bin Zakaria (Digital) <aripian.android@gmail.com>
2018-08-13 17:11:32 +0800

    fixed the script locations (please refer index.html)

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,13 +37,6 @@ module.exports = function (grunt) {
         files: ['bower.json'],
         tasks: ['wiredep']
       },
-      js: {
-        files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
-        tasks: ['newer:jshint:all', 'newer:jscs:all'],
-        options: {
-          livereload: '<%= connect.options.livereload %>'
-        }
-      },
       jsTest: {
         files: ['test/spec/{,*/}*.js'],
         tasks: ['newer:jshint:test', 'newer:jscs:test', 'karma']
